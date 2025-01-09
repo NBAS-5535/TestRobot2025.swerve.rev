982456d43be912556c4c72292fe8bf12166989ba
NBAS-5535 <uras@yahoo.com>
2025-01-08 18:43:26 -0600

    angle offset options

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -104,26 +104,26 @@ public final class Constants {
             new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
     // Angular offsets of the modules relative to the chassis in radians
-    /*
+    /**/
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
-    */
+    /*
     public static final double kFrontLeftChassisAngularOffset = 0;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = 0;
     public static final double kBackRightChassisAngularOffset = 0;
-
+    */
     // SPARK MAX CAN IDs
     public static final int kFrontLeftDrivingCanId = 1;
     public static final int kRearLeftDrivingCanId = 5;
-    public static final int kFrontRightDrivingCanId = 4;
+    public static final int kFrontRightDrivingCanId = 3;
     public static final int kRearRightDrivingCanId = 7;
 
     public static final int kFrontLeftTurningCanId = 2;
     public static final int kRearLeftTurningCanId = 6;
-    public static final int kFrontRightTurningCanId = 3;
+    public static final int kFrontRightTurningCanId = 4;
     public static final int kRearRightTurningCanId = 8;
 
     public static final boolean kGyroReversed = true; //false;
